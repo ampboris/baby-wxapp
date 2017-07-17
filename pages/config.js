abef2580e2f4c7f4de15f44da4f6c2737b089657
dj940212 <2902273280@qq.com>
2017-07-17 15:12:10 +0800

    完成照片视频宽高的获取

---
+++ pages/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    region: 'http://up-z2.qiniu.com', // 华南区
+    accessToken: '489cc410-13a9-4a0a-a73d-33fb0f2e3a6e',
+    domain: 'http://ot2nmqx5r.bkt.clouddn.com',
+
+    uptokenURL: 'http://localhost:1234/api/signature',
+    getPhotoVideoUrl: 'http://localhost:1234/api/photoVideo/list',
+    savePhotoVideoInfoUrl: 'http://localhost:1234/api/photoVideo/save',
+};
