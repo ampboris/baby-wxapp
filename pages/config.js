6ba4716a5a7fc905b359bcb8dcfc30fc30df0ee1
dj940212 <2902273280@qq.com>
2017-07-20 12:28:22 +0800

    添加样式

--- pages/config.js
+++ pages/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     region: 'http://up-z2.qiniu.com', // 华南区
-    accessToken: "8591137c-ff1c-4565-bf1f-19a540acde1b",
+    accessToken: "489cc410-13a9-4a0a-a73d-33fb0f2e3a6e",
     domain: 'http://ot2nmqx5r.bkt.clouddn.com',
 
     uptokenURL: 'http://localhost:1234/api/signature',
