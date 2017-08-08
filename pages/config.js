52a381eac4be2c81158c62dc7267bc00677c29f1
dj940212 <2902273280@qq.com>
2017-08-08 15:20:07 +0800

    发布照片页

--- pages/config.js
+++ pages/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     region: 'http://up-z2.qiniu.com', // 华南区
-    accessToken: "8591137c-ff1c-4565-bf1f-19a540acde1b",
+    accessToken: "e66b46bd-1445-4b90-a541-7dc07f41ca73",
     domain: 'http://ot2nmqx5r.bkt.clouddn.com',
 
-    uptokenURL: 'http://localhost:1234/api/signature',
-    getPhotoVideoUrl: 'http://localhost:1234/api/photoVideo/list',
-    savePhotoVideoInfoUrl: 'http://localhost:1234/api/photoVideo/save',
-    deletePhotoOrVideoUrl: 'http://localhost:1234/api/photoVideo/delete'
+    uptokenURL: 'https://baby.dingjian.name/api/signature',
+    getPhotoVideoUrl: 'https://baby.dingjian.name/api/photoVideo/list',
+    savePhotoVideoInfoUrl: 'https://baby.dingjian.name/api/photoVideo/save',
+    deletePhotoOrVideoUrl: 'https://baby.dingjian.name/api/photoVideo/delete'
 };
