3e95c93a40235582d0760c735a3d2610a6ecdfb5
dj940212 <2902273280@qq.com>
2017-08-09 17:00:21 +0800

    添加loading

--- pages/config.js
+++ pages/config.js
@@ -3,16 +3,16 @@ module.exports = {
     accessToken: "e66b46bd-1445-4b90-a541-7dc07f41ca73",
     domain: 'http://baby.dingjian.name',
 
-    // uptokenURL: 'https://dingjian.name/api/signature',
-    // getPhotoVideoUrl: 'https://dingjian.name/api/photoVideo/list',
-    // savePhotoVideoInfoUrl: 'https://dingjian.name/api/photoVideo/save',
-    // deletePhotoOrVideoUrl: 'https://dingjian.name/api/photoVideo/delete',
+    uptokenURL: 'https://dingjian.name/api/signature',
+    getPhotoVideoUrl: 'https://dingjian.name/api/photoVideo/list',
+    savePhotoVideoInfoUrl: 'https://dingjian.name/api/photoVideo/save',
+    deletePhotoOrVideoUrl: 'https://dingjian.name/api/photoVideo/delete',
 
 
-    uptokenURL: 'http://localhost:8084/api/signature',
-    getPhotoVideoUrl: 'http://localhost:8084/api/photoVideo/list',
-    savePhotoVideoInfoUrl: 'http://localhost:8084/api/photoVideo/save',
-    deletePhotoOrVideoUrl: 'http://localhost:8084/api/photoVideo/delete',
+    // uptokenURL: 'http://localhost:8084/api/signature',
+    // getPhotoVideoUrl: 'http://localhost:8084/api/photoVideo/list',
+    // savePhotoVideoInfoUrl: 'http://localhost:8084/api/photoVideo/save',
+    // deletePhotoOrVideoUrl: 'http://localhost:8084/api/photoVideo/delete',
     birthday: new Date(2016, 10, 27, 15, 0, 0),
 
 };
