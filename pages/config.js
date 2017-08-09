7b39c5199c7277bb33408c94b588d89e449c0a0c
dj940212 <2902273280@qq.com>
2017-08-09 09:37:04 +0800

    增加删除功能

--- pages/config.js
+++ pages/config.js
@@ -1,10 +1,17 @@
 module.exports = {
     region: 'http://up-z2.qiniu.com', // 华南区
     accessToken: "e66b46bd-1445-4b90-a541-7dc07f41ca73",
-    domain: 'http://ot2nmqx5r.bkt.clouddn.com',
+    domain: 'http://baby.dingjian.name',
 
-    uptokenURL: 'https://baby.dingjian.name/api/signature',
-    getPhotoVideoUrl: 'https://baby.dingjian.name/api/photoVideo/list',
-    savePhotoVideoInfoUrl: 'https://baby.dingjian.name/api/photoVideo/save',
-    deletePhotoOrVideoUrl: 'https://baby.dingjian.name/api/photoVideo/delete'
+    // uptokenURL: 'https://dingjian.name/api/signature',
+    // getPhotoVideoUrl: 'https://dingjian.name/api/photoVideo/list',
+    // savePhotoVideoInfoUrl: 'https://dingjian.name/api/photoVideo/save',
+    // deletePhotoOrVideoUrl: 'https://dingjian.name/api/photoVideo/delete',
+
+
+    uptokenURL: 'http://localhost:8084/api/signature',
+    getPhotoVideoUrl: 'http://localhost:8084/api/photoVideo/list',
+    savePhotoVideoInfoUrl: 'http://localhost:8084/api/photoVideo/save',
+    deletePhotoOrVideoUrl: 'http://localhost:8084/api/photoVideo/delete',
+    birthday: new Date(2016, 10, 27, 15, 0, 0)
 };
